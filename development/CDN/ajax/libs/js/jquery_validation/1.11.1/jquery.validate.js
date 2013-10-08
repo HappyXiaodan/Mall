861e1288c5e013f70bc0d9c32ab59fba488bf1ca
Tony-Stark <swzyocowboy@icloud.com>
2013-10-08 10:59:50 +0800

    Fixed format.

--- development/CDN/ajax/libs/js/jquery_validation/1.11.1/jquery.validate.js
+++ development/CDN/ajax/libs/js/jquery_validation/1.11.1/jquery.validate.js
@@ -240,54 +240,93 @@
 		
 		// http://docs.jquery.com/Plugins/Validation/rules
 		rules: function( command, argument ) {
+			
 			var element = this[0];
 			
 			if ( command ) {
+				
 				var settings = $.data(element.form, "validator").settings;
+				
 				var staticRules = settings.rules;
+				
 				var existingRules = $.validator.staticRules(element);
+				
 				switch(command) {
-				case "add":
-					$.extend(existingRules, $.validator.normalizeRule(argument));
-					// remove messages from rules, but allow them to be set separetely
-					delete existingRules.messages;
-					staticRules[element.name] = existingRules;
-					if ( argument.messages ) {
-						settings.messages[element.name] = $.extend( settings.messages[element.name], argument.messages );
-					}
-					break;
-				case "remove":
-					if ( !argument ) {
-						delete staticRules[element.name];
-						return existingRules;
-					}
-					var filtered = {};
-					$.each(argument.split(/\s/), function( index, method ) {
-						filtered[method] = existingRules[method];
-						delete existingRules[method];
-					});
-					return filtered;
+					
+					case "add":
+						
+						$.extend(existingRules, $.validator.normalizeRule(argument));
+						
+						// remove messages from rules, but allow them to be set separetely
+						delete existingRules.messages;
+						
+						staticRules[element.name] = existingRules;
+						
+						if ( argument.messages ) {
+							
+							settings.messages[element.name] = $.extend( settings.messages[element.name], argument.messages );
+							
+						}
+						
+						break;
+						
+					case "remove":
+						
+						if ( !argument ) {
+							
+							delete staticRules[element.name];
+							
+							return existingRules;
+							
+						}
+						
+						var filtered = {};
+						
+						$.each(argument.split(/\s/), function( index, method ) {
+							
+							filtered[method] = existingRules[method];
+							
+							delete existingRules[method];
+							
+						});
+						
+						return filtered;
+					
 				}
+				
 			}
 			
 			var data = $.validator.normalizeRules(
+				
 			$.extend(
+				
 				{},
+				
 				$.validator.classRules(element),
+				
 				$.validator.attributeRules(element),
+				
 				$.validator.dataRules(element),
+				
 				$.validator.staticRules(element)
+				
 			), element);
 			
 			// make sure required is at front
 			if ( data.required ) {
+				
 				var param = data.required;
+				
 				delete data.required;
+				
 				data = $.extend({required: param}, data);
+				
 			}
 			
 			return data;
+			
 		}
+		
 	});
 
 	// Custom selectors
