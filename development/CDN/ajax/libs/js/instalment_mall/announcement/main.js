2adcd194a97085edfeb5c8c8143df3d75d823c37
Tony-Stark <swzyocowboy@icloud.com>
2013-10-22 11:09:29 +0800

    We will give the details in the maintenance phase.

---
+++ development/CDN/ajax/libs/js/instalment_mall/announcement/main.js
@@ -0,0 +1,21 @@
+/**
+ * Statement: Just shut the fuck up!In case you hadn’t noticed, I’m a bit of a stickler for terminology.You motherfucker!!!
+ * 
+ * Describe: The javascript boot file of the announcement page (~/announcement/index.html).
+ * 
+ * Further changes, comments: ~
+ * 
+ * Docs: ~
+ * 
+ * Original Author: Tony ( Shen Weizhong ).
+ * 
+ * Version: 0.1.0
+ * 
+ * Creation Date: 2013.10.21 18:09 ( Tony ).
+ * 
+ * Last update: 2013.10.21 218:09 ( Tony ).
+ * 
+ * License: ~
+ * 
+ * Copyright: ~
+ */
