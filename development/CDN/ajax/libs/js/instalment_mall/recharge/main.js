b451ce8dd857bce4c7d43ceafa195e19b056a731
Tony-Stark <swzyocowboy@icloud.com>
2013-11-05 20:44:59 +0800

    We will give the details in the maintenance phase.

--- development/CDN/ajax/libs/js/instalment_mall/recharge/main.js
+++ development/CDN/ajax/libs/js/instalment_mall/recharge/main.js
@@ -21,201 +21,201 @@
  */
 
 (function (window, document, requirejs, require) {
-	
-	'use strict';
-	
-	var boot = boot || {};
-	
-	boot.getAgent = function () {
-		
-		return navigator.userAgent.toLowerCase();
-		
-	};
-	
-	boot.isIE = function(userAgent) {
-		
-		var agent = userAgent || this.getAgent();
-		
-		return !!agent.match(/msie/i);
-		
-	};
-	
-	boot.isGteIE9 = function(userAgent) {
-		
-		var agent = userAgent || this.getAgent(),
-			
-			match = agent.match(/msie\D*([\.\d]*)/i),
-			
-			version = 0;
-		
-		if (match && match[1]) {
-			
-			version = match[1];
-			
-		}
-		
-		return version >= 9;
-		
-	};
-	
-	boot.req = function (jquery) {
-		
-		requirejs.config({
-			
-			baseUrl: '../CDN',
-			
-			enforceDefine: false,
-			
-			paths: {
-				
-				'jquery': jquery,
-				
-				'cdnjs': 'ajax/libs/js'
-				
-			},
-			
-			waitSeconds: 60,
-			
-			map: {
-				
-				'*': {'jquery': 'cdnjs/jquery_private/jquery.private.min'},
-				
-				'cdnjs/jquery_private/jquery.private.min': {'jquery': 'jquery'}
-				
-			}
-			
-		});
-		
-		require([
-			
-			'cdnjs/modernizr_amd/modernizr.min',
-			
-			'jquery',
-			
-			'cdnjs/jquery_cookie/1.3.1/jquery.cookie.min',
-			
-			'cdnjs/gridder/0.1.0/gridder',
-			
-			'cdnjs/jquery_title_modify/title.modify'
-		
-		], function (modernizr, SJ, cookie, gridder, modifyTitle) {
-			
-			SJ(function ($) {
-				
-				var overlayObj = $('.chooseBank'),
-                    
-					closeBtn = $('.closeChoose'),
-                    
-					respondIpt = $('.selectedBank'),
-                    
-					overlayLink = $('.otherPaymentLink');
-
-
-
-				/**
-				 * click input's background image also can choose.
-				 */
+        
+        'use strict';
+        
+        var boot = boot || {};
+        
+        boot.getAgent = function () {
                 
-				overlayObj.find('.inputBlockLevel').on('click', function () {
-                    
-					$(this).children('input').attr('checked', true);
-                    
-				});
-
-
-
-				/**
-				 * click link to show overlay, choose bank.
-				 */
+                return navigator.userAgent.toLowerCase();
                 
-				overlayLink.on('click', function (e) {
-                    
-					e.preventDefault();
-                    
-					overlayObj.fadeIn(500).find('button').on('click', function (e) {
+        };
+        
+        boot.isIE = function(userAgent) {
+                
+                var agent = userAgent || this.getAgent();
+                
+                return !!agent.match(/msie/i);
+                
+        };
+        
+        boot.isGteIE9 = function(userAgent) {
+                
+                var agent = userAgent || this.getAgent(),
                         
-						var checkedObj = overlayObj.find('input[name=chooseBank]:checked'),
-							
-							checkedObjVal = checkedObj.val(),
+                        match = agent.match(/msie\D*([\.\d]*)/i),
+                        
+                        version = 0;
+                
+                if (match && match[1]) {
+                        
+                        version = match[1];
+                        
+                }
+                
+                return version >= 9;
+                
+        };
+        
+        boot.req = function (jquery) {
+                
+                requirejs.config({
+                        
+                        baseUrl: '../CDN',
+                        
+                        enforceDefine: false,
+                        
+                        paths: {
+                                
+                                'jquery': jquery,
+                                
+                                'cdnjs': 'ajax/libs/js'
+                                
+                        },
+                        
+                        waitSeconds: 60,
+                        
+                        map: {
+                                
+                                '*': {'jquery': 'cdnjs/jquery_private/jquery.private.min'},
+                                
+                                'cdnjs/jquery_private/jquery.private.min': {'jquery': 'jquery'}
+                                
+                        }
+                        
+                });
+                
+                require([
+                        
+                        'cdnjs/modernizr_amd/modernizr.min',
+                        
+                        'jquery',
+                        
+                        'cdnjs/jquery_cookie/1.3.1/jquery.cookie.min',
+                        
+                        'cdnjs/gridder/0.1.0/gridder',
+                        
+                        'cdnjs/jquery_title_modify/title.modify'
+                
+                ], function (modernizr, SJ, cookie, gridder, modifyTitle) {
+                        
+                        SJ(function ($) {
+                                
+                                var overlayObj = $('.chooseBank'),
+                    
+                                        closeBtn = $('.closeChoose'),
+                    
+                                        respondIpt = $('.selectedBank'),
+                    
+                                        overlayLink = $('.otherPaymentLink');
+
+
+
+                                /**
+                                 * click input's background image also can choose.
+                                 */
+                
+                                overlayObj.find('.inputBlockLevel').on('click', function () {
+                    
+                                        $(this).children('input').attr('checked', true);
+                    
+                                });
+
+
+
+                                /**
+                                 * click link to show overlay, choose bank.
+                                 */
+                
+                                overlayLink.on('click', function (e) {
+                    
+                                        e.preventDefault();
+                    
+                                        overlayObj.fadeIn(500).find('button').on('click', function (e) {
+                        
+                                                var checkedObj = overlayObj.find('input[name=chooseBank]:checked'),
+                                                        
+                                                        checkedObjVal = checkedObj.val(),
                             
-							checkedId = checkedObj.parent().attr('id');
+                                                        checkedId = checkedObj.parent().attr('id');
                         
-						e.preventDefault();
+                                                e.preventDefault();
                         
-						respondIpt.attr('id', checkedId).find('input').val(checkedObjVal);
+                                                respondIpt.attr('id', checkedId).find('input').val(checkedObjVal);
                         
-						overlayObj.fadeOut(500);
+                                                overlayObj.fadeOut(500);
                         
-					});
+                                        });
                     
-				});
+                                });
 
 
 
-				/**
-				 * click button to close overlay.
-				 */
+                                /**
+                                 * click button to close overlay.
+                                 */
                 
-				closeBtn.on('click', function (e) {
+                                closeBtn.on('click', function (e) {
                     
-					e.preventDefault();
+                                        e.preventDefault();
                     
-					overlayObj.fadeOut(500);
+                                        overlayObj.fadeOut(500);
                     
-				});
-				
-				
-				
-				/**
-				 * Development dependency: grid system.
-				 */
-				
-				gridder;
-				
-				
-				
-				/**
-				 * Title modify component( Public ) test.
-				 */
-				
-				modifyTitle();
-				
-			});
-			
-		});
-		
-	};
-	
-	boot.judgement = function (opts) {
-		
-		if (this.isIE()) {
-			
-			this.isGteIE9() ? this.req(opts.jq2x) : this.req(opts.jq1x);
-			
-		} else {
-			
-			this.req(opts.jq2x);
-			
-		}
-		
-	};
-	
-	/*             ___   ___
-				 \  \  \  \
-	   (___)   ___\__\__\__\__
-	   (o o)   |  O O O O O O|
-	  --\ /----+-------------+-------/
-	  |  O                          /
-	   \                           /
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ => moo__Steerage ...
-	*/
-	
-	boot.judgement({
-		
-		jq1x: 'ajax/libs/js/jquery/1.10.2/jquery.min',
-		
-		jq2x: 'ajax/libs/js/jquery/2.0.3/jquery.min'
-		
-	});
-		
+                                });
+                                
+                                
+                                
+                                /**
+                                 * Development dependency: grid system.
+                                 */
+                                
+                                gridder;
+                                
+                                
+                                
+                                /**
+                                 * Title modify component( Public ) test.
+                                 */
+                                
+                                modifyTitle();
+                                
+                        });
+                        
+                });
+                
+        };
+        
+        boot.judgement = function (opts) {
+                
+                if (this.isIE()) {
+                        
+                        this.isGteIE9() ? this.req(opts.jq2x) : this.req(opts.jq1x);
+                        
+                } else {
+                        
+                        this.req(opts.jq2x);
+                        
+                }
+                
+        };
+        
+        /*             ___   ___
+                                 \  \  \  \
+           (___)   ___\__\__\__\__
+           (o o)   |  O O O O O O|
+          --\ /----+-------------+-------/
+          |  O                          /
+           \                           /
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ => moo__Steerage ...
+        */
+        
+        boot.judgement({
+                
+                jq1x: 'ajax/libs/js/jquery/1.10.2/jquery.min',
+                
+                jq2x: 'ajax/libs/js/jquery/2.0.3/jquery.min'
+                
+        });
+                
 }(window, document, requirejs, require));
