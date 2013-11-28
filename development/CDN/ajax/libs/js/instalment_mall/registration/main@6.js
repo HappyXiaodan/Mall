e758e83f48231c0b288fa1480d3e6831c518f6e9
Tony-Stark <swzyocowboy@icloud.com>
2013-11-28 12:08:41 +0800

    updated registration progress five

--- development/CDN/ajax/libs/js/instalment_mall/registration/main@6.js
+++ development/CDN/ajax/libs/js/instalment_mall/registration/main@6.js
@@ -94,194 +94,664 @@
 			
 			'cdnjs/jquery_cookie/1.3.1/jquery.cookie.min',
 			
+			'cdnjs/jquery_scroll/0.1.0/jquery.scroll.min',
+			
+			'cdnjs/jquery_selector/0.1.0/jquery.selector',
+			
+			'cdnjs/jquery_validation/1.11.1/jquery.validate.min',
+			
 			'cdnjs/gridder/0.1.0/gridder',
 			
 			'cdnjs/jquery_title_modify/title.modify'
 		
-		], function (modernizr, SJ, cookie, gridder, modifyTitle) {
+		], function (modernizr, SJ, cookie, scro, sl, Hawaii, gridder, modifyTitle) {
 			
 			SJ(function ($) {
-
-
-				var selectorEd = $('.selectorEd'),
-
-					selectorPanel = $('.selectPanel'),
-
-					tgl = false;
 				
-				selectorEd.each(function (i, obj) {
-
-					$(this).on('click', function () {
-
-						var that = $(this),
-
-							panel = that.prev('div');
-
-						if (!tgl) {
-
-							if (!that.hasClass('selectorSchool')) {
-
-								that.addClass('selectorEdOpen');
-
+				
+				
+				var getOfficeInfo = (function ($, thisModule) {
+					
+					var privateFn,
+						
+						extendObject,
+						
+						tgl,
+						
+						utils,
+						
+						_validator,
+						
+						_shortArray,
+						
+						ctrlStatus,
+						
+						customSelector = $('.customSelector');
+					
+					privateFn = privateFn || {};
+					
+					extendObject = function (ns/*ns: Nested Namespace*/, nsString) {
+						
+						var part = nsString.split('.'),
+							
+							parent = ns,
+							
+							pl;
+						
+						pl = part.length;
+						
+						for (var i = 0; i < pl; i++) {
+							
+							if (typeof parent[part[i]] === 'undefined') {
+								
+								parent[part[i]] = {};
+								
 							}
-
-							that.css('z-index', 40);
-
-							that.parent().css('z-index', 2);
-
-							panel.removeClass('hide');
-
-							panel.animate({opacity: 1, top: 25}, {
-
-								duration: 400,
-
-								done: function () { tgl = true; }
-
+							
+						}
+						
+						return parent;
+						
+					};
+					
+					privateFn = extendObject(privateFn, 'privateFn.utils');
+					
+					privateFn.utils.åüdpControl = function (e) { //dp: preventDefault & stopPropagation.
+						
+						e.preventDefault();
+						
+						e.stopPropagation();
+						
+					};
+					
+					privateFn.utils.åüstopRightClick = function () {
+						
+						window.oncontextmenu = function () { // Not compatibile with IE < 9
+							
+							return false;
+							
+						};
+						
+					};
+					
+					privateFn = extendObject(privateFn, 'privateFn.schoolSwitch');
+					
+					privateFn.schoolSwitch.åüinit = function () {
+						
+						this.åütrigger();
+						
+					};
+					
+					privateFn.schoolSwitch.åütrigger = function () {
+						
+						var schoolPanelTrigger = $('.selectorSchool'),
+							
+							panel = $('.schoolPanel');
+						
+						var showPanel = function () {
+							
+							panel.find('.schoolListBox').niceScroll().show();
+							
+							panel.stop().animate({opacity: 1, top: 302}, {
+								
+								duration: 100,
+								
+								done: function () {
+									
+									tgl = true;
+									
+								}
+								
+							}).removeClass('hide');
+							
+						};
+						
+						var hidePanel = function () {
+							
+							panel.find('.schoolListBox').getNiceScroll().hide();
+							
+							panel.stop().animate({opacity: 0, top: 337}, {
+								
+								duration: 100,
+								
+								done: function () {
+									
+									tgl = false;
+									
+									panel.addClass('hide');
+									
+								}
+								
 							});
-
-						}
-
-					}).on('click', function () {
-
-						var that = $(this),
 							
-				 			panel = that.prev('div');
+						};
 						
-				 		if (tgl) {
+						schoolPanelTrigger.on('click.switch.tony', function () {
 							
-				 			that.removeClass('selectorEdOpen');
-
-				 			that.css('z-index', 1);
-
-				 			that.parent().css('z-index', 1);
+							!tgl ? showPanel() : hidePanel();
 							
-				 			panel.animate({opacity: 0, top: 35}, {
-								
-				 				duration: 400,
-								
-				 				done: function () {
-									
-				 					panel.addClass('hide');
-									
-				 					tgl = false;
-									
-				 				}
-								
-				 			});
-							
-						}
-
-					});
-
-				});
-
-
-
-				selectorPanel.each(function (i, obj) {
-
-					var _this = $(this)
-
-					_this.children('ul').find('li').on('click', function () {
-
-						var that = $(this),
-
-							selectedValue = that.text();
-
-						if (_this.parent().hasClass('selectEntYearArea') || _this.parent().hasClass('selectGraYearArea') ) {
-
-							_this.next('div').children('span').css('margin-left', -15).text(selectedValue);
-
-						} else {
-
-							_this.next('div').children('span').text(selectedValue);
-
-						}
-
-						_this.next('div').children('input').val(selectedValue);
-
-						_this.next('div').removeClass('selectorEdOpen');
-
-						_this.animate({ opacity: 0, top: 35 }, {
-
-							duration: 400,
-
-							done: function () {
-
-								selectorEd.prev('div').addClass('hide');
-
-								tgl = false;
-
-							}
-
-						})
-
-
-					});
-
-				});
-
-
-
-				$('.closeBtn').on('click', function (e) {
-
-					e.preventDefault();
-
-					$('.schoolPanel').animate({opacity: 0, top: 35}, {
-								
-		 				duration: 400,
-						
-		 				done: function () {
-							
-		 					$(this).addClass('hide');
-							
-		 					tgl = false;
-							
-		 				}
-						
-		 			});
-
-				});
-
-
-
-
-				/**
-				 * school panel.
-				 */
-
-				var cityListObj = $('.cityList').find('a'),
-
-					areaListObj = $('.areaList').find('a'),
-
-					method = {};
-
-				method = {
-
-					clickEvent: function (obj, className) {
-
-						obj.on('click', function (e) {
-
-							e.preventDefault();
-
-							e.stopPropagation();
-
-							obj.parent().removeClass(className);
-
-							$(this).parent().addClass(className);
-
 						});
-
-					}
-
-				};
-
-				method.clickEvent(cityListObj, 'currentCity');
-
-				method.clickEvent(areaListObj, 'currentArea')
-
-
+						
+						$('.closeBtn').on('click.closepanel.tony', hidePanel);
+						
+						$('.schoolListBox').on('click', 'a', function (e) {
+							
+							privateFn.utils.åüdpControl(e);
+							
+							schoolPanelTrigger.children('span').text($(this).text());
+							
+							if (schoolPanelTrigger.data('modify') == false) {
+								
+								schoolPanelTrigger.data('modify', true);
+								
+								console.log(schoolPanelTrigger.data('modify'));
+								
+							}
+							
+							/*
+							 * Stop for short time.
+							 * 
+							 *schoolPanelTrigger.css({
+								
+								'left': '50%',
+								
+								'margin-left': -($('.selectorSchool').width()/2)
+								
+							});*/
+							
+						});
+						
+					};
+					
+					privateFn = extendObject(privateFn, 'privateFn.valior');
+					
+					privateFn.valior.åüinit = function () {
+						
+						this.åüplugin();
+						
+						this.åünext();
+						
+						this.åüoriginal();
+						
+					};
+					
+					privateFn.valior.åüplugin = function () {
+						
+						$.validator.setDefaults({
+							
+							debug: true,
+							
+							onfocusout: function(element) {
+								
+								var invalidCount = _validator.numberOfInvalids();
+								
+								_shortArray = [];
+								
+								SJ(element).valid();
+								
+								customSelector.each(function () {
+									
+									var _that = $(this);
+									
+									if (_that.hasClass('selectorSchool')) {
+										
+										if (_that.data('modify') == false) {
+											
+											_shortArray.push(_that.attr('class'));
+											
+										}
+										
+									} else if (_that.find('.slt_selected').children().data('modify') == false) {
+										
+										_shortArray.push(_that.data('target'));
+										
+									}
+									
+									if (invalidCount == 0 && _shortArray.length == 0) {
+										
+										$('.infoTool').children('button').removeClass('disable');
+										
+									} else {
+										
+										$('.infoTool').children('button').addClass('disable');
+										
+									}
+									
+								});
+								
+								console.log('Invalid count: ' + invalidCount + '. ' + _shortArray);
+								
+							},
+							
+							onkeyup: function(element) {
+								
+								var invalidCount = _validator.numberOfInvalids();
+								
+								_shortArray = [];
+								
+								SJ(element).valid();
+								
+								customSelector.each(function () {
+									
+									var _that = $(this);
+									
+									if (_that.hasClass('selectorSchool')) {
+										
+										if (_that.data('modify') == false) {
+											
+											_shortArray.push(_that.attr('class'));
+											
+										}
+										
+									} else if (_that.find('.slt_selected').children().data('modify') == false) {
+										
+										_shortArray.push(_that.data('target'));
+										
+									}
+									
+									if (invalidCount == 0 && _shortArray.length == 0) {
+										
+										$('.infoTool').children('button').removeClass('disable');
+										
+									} else {
+										
+										$('.infoTool').children('button').addClass('disable');
+										
+									}
+									
+								});
+								
+								console.log('Invalid count: ' + invalidCount + '. ' + _shortArray.length);
+								
+							},
+							
+							success: function(error) {
+								
+								SJ(error).remove();
+								
+							},
+							
+							errorElement: 'div'
+							
+						});
+						
+						$.validator.addMethod("nowhitespace", function(value, element) {
+							
+							return this.optional(element) || /^\S+$/i.test(value);
+							
+						}, "不允许有空格。");
+						
+						$.validator.addMethod("Chinses", function(value, element) {
+							
+							return this.optional(element) || /[^x00-xff]/.test(value);
+							
+						}, "请输入中文。");
+						
+						$.validator.addClassRules({
+							
+							company: {
+								
+								required: true
+								
+							},
+							
+							income: {
+								
+								required: true
+								
+							},
+							
+							sector: {
+								
+								required: true
+								
+							},
+							
+							jobTitle: {
+								
+								required: true
+								
+							},
+							
+							comPhoneAreaCode: {
+								
+								required: true
+								
+							},
+							
+							comPhone: {
+								
+								required: true
+								
+							},
+							
+							comAddressDetail: {
+								
+								required: true
+								
+							},
+							
+							homeAddressDetail: {
+								
+								required: true
+								
+							},
+							
+							currAddressDetail: {
+								
+								required: true
+								
+							}
+							
+						});
+						
+						var fn = function (obj) {
+							
+							$('.sass').each(function (index) {
+								
+								if (!$(this).hasClass('hide')) {
+									
+									console.log(index);
+									
+									return false;
+									
+								} else if (index == $('.sass').length - 1) {
+									
+									console.log(index);
+									
+									obj.init();
+									
+								}
+								
+							});
+							
+						};
+						
+						_validator = $('#frmSuppInfo').validate({
+							
+							errorPlacement: function (error, element) {
+								
+								switch (SJ(element).attr('id')) {
+									
+									case 'company': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg companyErrorPos');
+										
+										SJ(element).siblings('.lblComID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'income': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg incomeErrorPos');
+										
+										SJ(element).siblings('.lblIncomeID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'sector': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg sectorError');
+										
+										SJ(element).siblings('.lblSectorID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'jobTitle': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg jobError');
+										
+										SJ(element).siblings('.lblJobID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'comPhoneAreaCode': {
+										
+										SJ(error).addClass('absol absol textCenter box_round errMsg phoneError areaCodeErrorPos');
+										
+										SJ(element).siblings('.lblTeleID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'comPhone': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg phoneError teleErrorPos');
+										
+										SJ(element).siblings('.lblTeleID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'comAddressDetail': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg comAddError lightGreyBg');
+										
+										SJ(element).siblings('.lblComAdrID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'homeAddressDetail': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg homeDetailAddError lightGreyBg');
+										
+										SJ(element).siblings('.lblHomeAdrID').append(error);
+										
+										break;
+										
+									}
+									
+									case 'currAddressDetail': {
+										
+										SJ(error).addClass('absol textCenter box_round errMsg nowAddDetailError lightGreyBg');
+										
+										SJ(element).siblings('.lblNowAdrID').append(error);
+										
+										break;
+										
+									}
+									
+								}
+								
+							},
+							
+							submitHandler: function (form, event) {
+								
+								var _sub = (function (_mod) {
+									
+									_mod.init = function () {
+										
+										console.log('Right.');
+										
+									};
+									
+									return _mod;
+									
+								} (_sub || {}));
+								
+								fn(_sub);
+								
+							}
+							
+						});
+						
+					};
+					
+					privateFn.valior.åünext = function () {
+						
+						var ctmSltEachFn = function (index) {
+							
+							var that = $(this),
+								
+								dataArea, defaultValue, currentValue, status;
+							
+							if (that.hasClass('selectorSchool')) {
+								
+								var dv = that.data('default'),
+									
+									cv = that.children('span').text();
+								
+								if (cv == dv) {
+									
+									that.siblings('.schoolError').removeClass('hide');
+									
+								}
+								
+							} else {
+								
+								dataArea = that.find('.slt_selected').children();
+								
+								defaultValue = dataArea.data('default');
+								
+								currentValue = dataArea.text();
+								
+								status = (currentValue == defaultValue && dataArea.data('modify') == false);
+								
+								if (status) {
+									
+									var errorTarget = that.data('target');
+									
+									$('.'+errorTarget).removeClass('hide');
+									
+								}
+								
+							}
+							
+						};
+						
+						$('.infoTool').children('button').on('click.next.tony', function () {
+							
+							customSelector.each(ctmSltEachFn);
+							
+						});
+						
+					};
+					
+					privateFn.valior.åüoriginal = function () {
+						
+						customSelector.each(function (index) {
+							
+							var that = $(this),
+								
+								selected,
+								
+								option;
+							
+							if (that.hasClass('selectorSchool')) {
+								
+								that.on('click.holy.tony', function () {
+									
+									if (!that.siblings('.schoolError').hasClass('hide')) {
+										
+										$(this).siblings('.schoolError').addClass('hide');
+										
+									}
+									
+								});
+								
+							} else {
+								
+								selected = that.find('.slt_selected');
+								
+								option = that.find('li');
+								
+								selected.on('click.slt_selected.tony', function () {
+									
+									var errorTarget = that.data('target');
+									
+									if (!$('.'+errorTarget).hasClass('hide')) {
+										
+										$('.'+errorTarget).addClass('hide');
+										
+									}
+									
+								});
+								
+								option.on('click.options.tony', function () {
+									
+									var _this = $(this),
+										
+										dataModify = _this.offsetParent().siblings('.slt_selected').children();
+									
+									_shortArray  = [];
+									
+									if (dataModify.data('modify') == false) {
+										
+										dataModify.data('modify', true);
+										
+										console.log(that.find('.slt_selected').children().data('modify'));
+										
+									}
+									
+									var invalidCount = _validator.numberOfInvalids();
+									
+									customSelector.each(function () {
+										
+										var _that = $(this);
+										
+										if (_that.hasClass('selectorSchool')) {
+											
+											if (_that.data('modify') == false) {
+												
+												_shortArray.push(_that.attr('class'));
+												
+											}
+											
+										} else if (_that.find('.slt_selected').children().data('modify') == false) {
+											
+											_shortArray.push(_that.data('target'));
+											
+										}
+										
+										if (invalidCount == 0 && _shortArray.length == 0) {
+											
+											$('.infoTool').children('button').removeClass('disable');
+											
+										} else {
+											
+											$('.infoTool').children('button').addClass('disable');
+											
+										}
+										
+									});
+									
+								});
+								
+							}
+							
+						});
+						
+					};
+					
+					thisModule.init = function () {
+						
+						$('.slt').slt();
+						
+						this.utils.åüstopRightClick();
+						
+						this.schoolSwitch.åüinit();
+						
+						this.valior.åüinit();
+						
+					}.bind(privateFn);
+					
+					return thisModule;
+					
+				} ($, getOfficeInfo || {})).init();
 				
-
+				
 				
 				/**
 				 * Development dependency: grid system.
